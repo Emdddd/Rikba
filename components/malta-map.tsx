045e59e7f794821a537b59d6cf493f3775f902bb
Emdddd <94723826+Emdddd@users.noreply.github.com>
2026-02-28 03:51:03 +0100

    Update malta-map.tsx

--- components/malta-map.tsx
+++ components/malta-map.tsx
@@ -22,41 +22,45 @@ export function MaltaMap({ className = "" }: { className?: string }) {
           "
         />
 
-        {/* ========================= */}
-        {/* PULSE LOCATIONS */}
-        {/* ========================= */}
-
-        {/* Valletta */}
-        <div className="map-dot" style={{ top:"58%", left:"63%" }} />
-        <div className="map-pulse" style={{ top:"58%", left:"63%" }} />
-
-        {/* Sliema */}
-        <div className="map-dot" style={{ top:"54%", left:"60%" }} />
-        <div className="map-pulse" style={{ top:"54%", left:"60%", animationDelay:"0.6s" }} />
-
-        {/* St Julian's */}
-        <div className="map-dot" style={{ top:"49%", left:"62%" }} />
-        <div className="map-pulse" style={{ top:"49%", left:"62%", animationDelay:"1.2s" }} />
-
-        {/* Airport Area */}
-        <div className="map-dot" style={{ top:"66%", left:"55%" }} />
-        <div className="map-pulse" style={{ top:"66%", left:"55%", animationDelay:"1.8s" }} />
-
-        {/* Mdina */}
-        <div className="map-dot" style={{ top:"45%", left:"48%" }} />
-        <div className="map-pulse" style={{ top:"45%", left:"48%", animationDelay:"2.4s" }} />
-
-        {/* Mellieha */}
-        <div className="map-dot" style={{ top:"30%", left:"55%" }} />
-        <div className="map-pulse" style={{ top:"30%", left:"55%", animationDelay:"3s" }} />
+        {/* ===================== */}
+        {/* 9 DISTRIBUTED DOTS */}
+        {/* ===================== */}
 
         {/* Gozo */}
-        <div className="map-dot" style={{ top:"12%", left:"42%" }} />
-        <div className="map-pulse" style={{ top:"12%", left:"42%", animationDelay:"3.6s" }} />
+        <div className="map-dot" style={{ top:"14%", left:"42%" }} />
+        <div className="map-pulse" style={{ top:"14%", left:"42%" }} />
 
-        {/* South Malta */}
-        <div className="map-dot" style={{ top:"78%", left:"50%" }} />
-        <div className="map-pulse" style={{ top:"78%", left:"50%", animationDelay:"4.2s" }} />
+        {/* North Malta */}
+        <div className="map-dot" style={{ top:"30%", left:"56%" }} />
+        <div className="map-pulse" style={{ top:"30%", left:"56%", animationDelay:"0.4s" }} />
+
+        {/* West */}
+        <div className="map-dot" style={{ top:"44%", left:"46%" }} />
+        <div className="map-pulse" style={{ top:"44%", left:"46%", animationDelay:"0.8s" }} />
+
+        {/* Center */}
+        <div className="map-dot" style={{ top:"50%", left:"58%" }} />
+        <div className="map-pulse" style={{ top:"50%", left:"58%", animationDelay:"1.2s" }} />
+
+        {/* Valletta Area */}
+        <div className="map-dot" style={{ top:"56%", left:"66%" }} />
+        <div className="map-pulse" style={{ top:"56%", left:"66%", animationDelay:"1.6s" }} />
+
+        {/* South East */}
+        <div className="map-dot" style={{ top:"66%", left:"63%" }} />
+        <div className="map-pulse" style={{ top:"66%", left:"63%", animationDelay:"2s" }} />
+
+        {/* Airport / Central South */}
+        <div className="map-dot" style={{ top:"66%", left:"53%" }} />
+        <div className="map-pulse" style={{ top:"66%", left:"53%", animationDelay:"2.4s" }} />
+
+        {/* South West */}
+        <div className="map-dot" style={{ top:"74%", left:"48%" }} />
+        <div className="map-pulse" style={{ top:"74%", left:"48%", animationDelay:"2.8s" }} />
+
+        {/* Far South */}
+        <div className="map-dot" style={{ top:"82%", left:"55%" }} />
+        <div className="map-pulse" style={{ top:"82%", left:"55%", animationDelay:"3.2s" }} />
 
       </div>
     </div>
